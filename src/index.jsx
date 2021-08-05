3c585b845baa68093fbfe4364ad9210a6bd93c71
Gilberto Cao <gilbe.cao@gmail.com>
2021-08-05 21:28:50 +0200

    Connect components and provide redux store

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
+import configureStore from './redux/stores';
+import initialState from './redux/stores/initialState';
+import SearchResults from './components/SeachResults';
+import BeerList from './components/BeerList';
+import RandomBeer from './components/RandomBeer';
+import SearchFilters from './components/SearchFilters';
+
+const BeerSearchResult = SearchResults(BeerList);
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Punk API Demo</h1>
+    <Provider store={configureStore(initialState)}>
+      <h1>Punk API Demo</h1>
+      <RandomBeer />
+      <SearchFilters />
+      <BeerSearchResult />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
